3256941775d635d0125af3651e9c39b715fa0b99
LittlePage <littlepageprogram@outlook.com>
2019-07-02 02:50:09 +0800

    增加会员卡购买

--- CinemaSystem/src/main/java/com/littlepage/controller/ClubController.java
+++ CinemaSystem/src/main/java/com/littlepage/controller/ClubController.java
@@ -1,14 +1,47 @@
 package com.littlepage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/club")
 public class ClubController {
 
+		/**
+		 * 购买会员卡页面
+		 * @return
+		 */
 		@RequestMapping("/buycard")
 		public String buycard() {
 			return "common/clubcard/buycard";
+		}
+		
+		/**
+		 * 支付方式页面
+		 * @param price
+		 * @param model
+		 * @return
+		 */
+		@RequestMapping("/payMethod")
+		public String payMethod(@RequestParam("price")int price,Model model) {
+			model.addAttribute("price",price);
+			return "common/clubcard/payMethod";
+		}
+		
+		/**
+		 * 支付密码页面
+		 * @param price
+		 * @param model
+		 * @param payMethod
+		 * @return
+		 */
+		@RequestMapping("/payPassword")
+		public String payPassword(@RequestParam("price")int price,Model model,
+				@RequestParam("payMethod")String payMethod) {
+			model.addAttribute("price",price);
+			model.addAttribute("payMethod",payMethod);//alipay or wechat pay
+			return "/pay/paymentPage";
 		}
 }
